eb128f9cc2ac7f54964ee86c80fa3d1b6cac887e
Shane Buckley <tshanebuckley@gmail.com>
2023-09-05 21:56:36 -0400

    basic dynamic Json componenent

---
+++ Components/ComponentDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    class ComponentDto
+    {
+        public string TypeName { get; set; }
+        public Dictionary<string, ParameterValueDto> Parameters
+        { get; set; }
+    }
+    class ParameterValueDto
+    {
+        public string TypeName { get; set; }
+        public object Value { get; set; }
+    }
+    class ComponentDescription
+    {
+        public Type ComponentType { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+    }
+    static class ComponentDtoToComponent
+    {
+        public static ComponentDescription ToComponent(
+            this ComponentDto dto)
+        {
+            return new ComponentDescription
+            {
+                ComponentType = Type.GetType(dto.TypeName),
+                Parameters = dto.Parameters?.ToDictionary(
+                    pp => pp.Key,
+                    pp => pp.Value != null
+                        ? JsonSerializer.Deserialize(
+                            ((JsonElement)pp.Value.Value).GetRawText(),
+                            Type.GetType(pp.Value.TypeName))
+                        : null)
+                    ?? new Dictionary<string, object>()
+            };
+        }
+    }
+}
